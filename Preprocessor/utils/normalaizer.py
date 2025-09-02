d774c339a89f70f2be50355203e7343121aafd6e
Policken66 <komlev.artem.02@mail.ru>
2025-09-02 21:33:46 +0700

    Вернул данные препроцессора в главную ветку.

---
+++ Preprocessor/utils/normalaizer.py
@@ -0,0 +1,16 @@
+from preprocessor.config.schema import ConfigModel
+
+
+class NormalizationError(Exception):
+    pass
+
+
+def to_si(cfg: ConfigModel) -> ConfigModel:
+    """
+    Приводит значения конфигурации к СИ (сейчас pass-through).
+    Если в будущем появятся поля с единицами, выполняем конвертацию здесь.
+    :param cfg:
+    :return:
+    """
+
+    return cfg
