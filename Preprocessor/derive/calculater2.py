0f13d204a8f26d158eb6f5699e9a73e2a0909328
Lida <lol.petrushka@mail.ru>
2025-10-27 16:09:19 +0700

    а

--- Preprocessor/derive/calculater2.py
+++ Preprocessor/derive/calculater2.py
@@ -4,24 +4,25 @@ import numpy as np
 
 
 def algM():
-    N = np.array([10, 20, 30])*2
-    alp = np.array([10, 15,20])*math.pi/180
+    N = np.array([30,45,60,75,90])*2
+    alp = np.array([14,16,18,20,22,24])*math.pi/180
 
 
-    n=np.array([[2,5,2],[15,5,5],[0.2,5,8]])
+
+    #n=np.array([[2,5,2],[15,5,5],[0.2,5,8]])
     #y=n.transpose()
 
 
 
-    print(alp)
+    #print(alp)
     #print(N*alp)
     #print(N[1])
 
-    for i, value in enumerate(N):
-       print(f"N[{i}] = {value}")
+   # for i, value in enumerate(N):
+       #print(f"N[{i}] = {value}")
 
-    for i, value in enumerate(alp):
-        print(f"alp[{i}] = {value}")
+#    for i, value in enumerate(alp):
+       # print(f"alp[{i}] = {value}")
 
 
     #for i, value_alp in enumerate(alp):
@@ -44,6 +45,7 @@ def algM():
     V2=N_kol*math.pi*D/2*b[1]*h[1]*2
     V3=N_chp*math.pi*D/2*2*b[2]*h[2]
     V=V1+V2+V3
+    #print(V2)
 
     M1=ro*V1
     M2 = ro * V2
@@ -53,18 +55,30 @@ def algM():
 
     for i,value_alp_ in enumerate(alp):
         for j, value_N_ in enumerate(N):
-            mmm=(H/(2*math.pi*D/2))*(N[j]*math.tan(alp[i]))
-            mm=math.ceil(mmm)*2
-            print(math.ceil(mm))
-    #print(N[1]*alp)
+            for k, value_N_ in enumerate(h):
+             mmm=(H/(math.pi*D))*(N[0]*math.tan(18*math.pi/180))
+             mm=math.ceil(mmm)-k
+           # print(math.ceil(mm))
+           # print(mm)
 
     #вычисление длин спиральных ребер
     for i, value_alp_ in enumerate(alp):
         L = H/math.cos(alp[i])
-        print(L)
-    #расчет толщин ребер при фиксированной высоте
+        #print(L)
+    #расчет спиральных толщин ребер при фиксированной высоте
 
-    
+    for i, value_alp_ in enumerate(alp):
+        for j, value_alp_ in enumerate(N):
+            bb_sp=V1/(N[0]*h[0]*H/math.cos(alp[i]))
+            mmm = (H / (2 * math.pi * D / 2)) * (N[j] * math.tan(alp[i]))/2
+            mm = math.ceil(mmm)
+            bb_k = V2 / (mm *2* math.pi * D / 2 * h[1])
+            bb_=(bb_k+bb_sp)/2
+            print(mm)
+    # расчет кольцевых толщин ребер при фиксированной высоте )
+
+    #for i in range(0, enumerate(N)):
+    #    print(i)
 
 
 
@@ -73,12 +87,8 @@ def algM():
 
 
 
-
-    x=np.linalg.solve(n,alp)
+   # x=np.linalg.solve(n,alp)
    # print(x)
-
-
-
 
 
 
